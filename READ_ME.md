76d4e7a6249ce513d484d5e5914d15c7d2081032
Ileini Melendez <122940019+ileini-melendez@users.noreply.github.com>
2023-03-16 14:58:33 +0000

    Initial commit

---
+++ READ_ME.md
@@ -0,0 +1,22 @@
+# JavaScript Lesson 1.2 Template - Greeting Program
+
+### Welcome! 
+Instructions for cloning down this repository to an IDE (_coding environment_):
+  1. Above the list of files, click on the `Code` button.
+  2. Copy the URL of the repository.
+  3. If you are using Repl.it:
+    * Select the `+ Create Repl` button on the top left corner of the web page.
+    * In the pop up window, select the `Import from GitHub` button
+    * Paste the GitHub URL and click on the button `Import from GitHub`.
+
+Note: If you are coding along with the video, we suggest you open the video in a window on the left and your code in a window on the right. This method of coding along is called "split screen".
+
+#### Using this template, you will be able to practice the following:
+
+- Using various logical operators to code conditional statements.
+- Ask for user input and use conditional statments to print custom messages to the console.
+- Code complex conditional statements that compare various data types.
+
+#### Get Started:
+
+- Click on the file named "script.js" to get started.
---
+++ script.js
@@ -0,0 +1,16 @@
+// --------------------------------------------------------------
+// Logical  Operators '===' --> Code your conditional statement using '===' to greet our 'Beyonce' user or a new user below.
+
+
+// --------------------------------------------------------------
+// Logical  Operators '>' and '<' --> Code your conditional statement using '>=' to check if our user is old enough to vote below.
+
+
+// Mini Challenge --> OPTIONS: (1)Add to the age check program by including other cutoffs for activities like driving, renting a car or retiring. (2)Go back to the updated greeting program above and create custom console.log statements to greet other celebrities. Type your code below.
+
+
+// --------------------------------------------------------------
+// Complex Conditionals --> Code your complex conditional to check if 'Beyonce' logs in with a correct or incorrect password AND welcomes a new user below. 
+
+
+// Mini Challenge --> OPTIONS: (1)Create accounts for other celebrities and create conditional statements that check if their name and passwords match. (2)If a user does not currently have an account in our updated greeting program, allow them the option to create a password for their account. Type your code below.
